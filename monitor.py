5e6895de0e1fee3d93b378bdae6b29cfb1311474
Meto1998-byte <Mahmoudmetwly58@icloud.com>
2026-07-02 18:11:27 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -6,11 +6,13 @@ URL = "https://services.rcsed.ac.uk/exams/exam-details-membership-in-orthodontic
 BOT_TOKEN = "8841127547:AAH3h1DLJIVrz7Vphi050tdPF4auYOcVhLM"
 CHAT_ID = "676405292"
 
+
 def send(msg):
     requests.post(
         f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
         data={"chat_id": CHAT_ID, "text": msg},
     )
+
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
